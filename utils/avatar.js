1edc42f56c711cb712f2673bc1468668bbc283f3
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-28 14:31:48 +0700

    r

--- utils/avatar.js
+++ utils/avatar.js
@@ -35,7 +35,7 @@ const penerima = [
   "6285183819833@c.us", //robot
   // "6281220000306@c.us", // pa sahmudin
   //"6281224733362@c.us", // risma
-  // "6281806000781@c.us", //yanti
+  "6281806000781@c.us", //yanti
   // "6282124609104@c.us", // pa Er
 ];
 
