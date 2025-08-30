26f8fcc847ada5d8f6353637f76bc432dd7a2906
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-30 13:58:35 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
@@ -7,25 +6,14 @@ const client = new Client({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
-  // webVersionCache: {
-  //   type: "remote",
-  //   remotePath:
-  //     "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-  // },
-  webVersionCache: {
-    type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2415.5.html",
-  },
 });
 
 client.on("qr", (qr) => {
-  console.log("QR RECEIVED", qr);
-  qrcode.generate(qr, { small: true });
+  console.log("QR RECEIVED:", qr);
 });
 
 client.on("ready", () => {
-  console.log("✅ WhatsApp ready!");
+  console.log("✅ Client is ready!");
 });
 
 client.initialize();
