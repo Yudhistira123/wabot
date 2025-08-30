ea7475a40ecd928999d33737457bde794a9f9d45
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-30 13:11:56 +0700

    test

--- test.js
+++ test.js
@@ -1,17 +1,9 @@
 const puppeteer = require("puppeteer");
+
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-gpu",
-    ],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   console.log("Chromium launched OK!");
   await browser.close();
