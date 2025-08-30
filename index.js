751cb6cf9da68e3706237af26464033f42fb7080
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-30 13:50:02 +0700

    test

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const client = new Client({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
+  // webVersionCache: {
+  //   type: "remote",
+  //   remotePath:
+  //     "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  // },
   webVersionCache: {
     type: "remote",
     remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2415.5.html",
   },
 });
 
