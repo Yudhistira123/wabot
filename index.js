a570919c3475f4c17dc0f4ab79ecf033bb516ec5
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-30 13:40:13 +0700

    test

--- index.js
+++ index.js
@@ -68,6 +68,11 @@ const client = new Client({
       "--disable-gpu",
       "--disable-software-rasterizer",
     ],
+    webVersionCache: {
+      type: "remote",
+      remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
   },
 });
 //initMQTT(client);
