51b57a47cc52f55157e9fa8586eaf5001beff3f1
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-08-30 13:14:44 +0700

    test

--- index.js
+++ index.js
@@ -49,16 +49,17 @@ const client = new Client({
   authStrategy: new LocalAuth({ clientId: "session-yudhi-boot" }),
   puppeteer: {
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-gpu",
-    ],
+    // args: [
+    //   "--no-sandbox",
+    //   "--disable-setuid-sandbox",
+    //   "--disable-dev-shm-usage",
+    //   "--disable-accelerated-2d-canvas",
+    //   "--no-first-run",
+    //   "--no-zygote",
+    //   "--single-process",
+    //   "--disable-gpu",
+    // ],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
 });
 initMQTT(client);
