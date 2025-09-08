b21cea75427abbc5187512fe011525c1a343ceac
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-09-08 14:52:27 +0700

    test

--- utils/sholat.js
+++ utils/sholat.js
@@ -82,7 +82,7 @@ export async function getSholatByLocation(kodeLokasi) {
       `☀️ Dzuhur    : ${jadwal.dzuhur} WIB\n` +
       `🌇 Ashar     : ${jadwal.ashar} WIB\n` +
       `🌆 Maghrib   : ${jadwal.maghrib} WIB\n` +
-      `🌙 Isya     : ${jadwal.isya} WIB`;
+      `🌙 Isyaxx     : ${jadwal.isya} WIB`;
     //======
 
     // console.log(res.data);
