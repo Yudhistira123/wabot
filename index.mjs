693f29775d5e8805a34c1314052d9feb663275a0
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-09-11 08:54:05 +0700

    x

--- index.mjs
+++ index.mjs
@@ -192,12 +192,12 @@ async function startBot() {
         const places = await getFilteredPOISorted(latitude, longitude, 1000);
 
         // Format semua hasil jadi satu string
-        let replyMsg3 = "📍 *Hasil Penting Saat Turing*:\n\n";
+        let replyMsg3 = "📍 *Tempat Penting Saat Turing*:\n\n";
         places.slice(0, 20).forEach((p, i) => {
           const mapsLink = `https://www.google.com/maps?q=${p.lat},${p.lon}`;
           replyMsg3 += `${i + 1}. ${p.name} - 📏 ${
             p.distance_km
-          } km\n👉${mapsLink}\n\n`;
+          } km\n${mapsLink}\n\n`;
         });
 
         await sock.sendMessage(from, {
