414d8ab8510a0f81477df49e09f961dfa12a0b4b
Yudhistira123 <yudhistira.sulaeman@gmail.com>
2025-09-27 07:32:23 +0700

    xx

--- utils/mqttService.js
+++ utils/mqttService.js
@@ -1,23 +1,27 @@
 //const axios = require("axios");
 import axios from "axios";
 
-const numbers = [
-  "628122132341@c.us",
-  "6285183819833@c.us",
-  // "6285220757725@c.us",
-  // "628122233610@c.us",
-  // "6285975386345@c.us",
-  // "628121462983@c.us",
-];
-
 export async function sendMessages(client, topic, message) {
-  for (const number of numbers) {
+  const text = `Lampu ${topic} : ${message.toString()}`;
+  //const jids = ["628122132341@c.us", "6285183819833@c.us"]; // your WA targets
+  const jids = [
+    "628122132341@c.us",
+    "6285183819833@c.us",
+    // "6285220757725@c.us",
+    // "628122233610@c.us",
+    // "6285975386345@c.us",
+    // "628121462983@c.us",
+  ];
+  for (const jid of jids) {
     try {
-      await client.sendMessage(
-        number,
-        ` Lampu ${topic} : ${message.toString()}`
-      );
-      console.log(`✅ Message sent to ${number}`);
+      await client.sendMessage(jid, { text }); // <-- FIX: must be { text: "..." }
+
+      // await client.sendMessage(
+      //   number,
+      //   ` Lampu ${topic} : ${message.toString()}`
+      // );
+      console.log(`✅ Sent to ${jid}: ${text}`);
+      //   console.log(`✅ Message sent to ${number}`);
     } catch (err) {
       console.error(`❌ Failed to send to ${number}:`, err);
     }
